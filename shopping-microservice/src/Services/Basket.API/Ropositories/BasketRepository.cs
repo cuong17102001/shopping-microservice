361c7302de6e3790779f113bb420d1bd835c5e69
cuongnq <nqcuong.1710@gmail.com>
2024-08-17 23:48:45 +0700

    remove tracking file

--- shopping-microservice/src/Services/Basket.API/Ropositories/BasketRepository.cs
+++ shopping-microservice/src/Services/Basket.API/Ropositories/BasketRepository.cs
@@ -1,26 +1,50 @@
 ﻿using Basket.API.Entities;
 using Basket.API.Ropositories.Interfaces;
+using Contracts.Common.Interfaces;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace Basket.API.Ropositories
 {
     public class BasketRepository : IBasketRepository
     {
-        private readonly IDistributedCache _distributedCache;
+        private readonly IDistributedCache _redisCacheService;
+        private readonly ISerializeService _serializerService;
 
-        public Task DeleteBasketByUsername(string username)
+        public BasketRepository(IDistributedCache distributedCache, ISerializeService serializeService)
         {
-            throw new NotImplementedException();
+            _redisCacheService = distributedCache;
+            _serializerService = serializeService;
         }
 
-        public Task<Cart?> GetBasketByUsername(string username)
+        public async Task DeleteBasketByUsername(string username)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _redisCacheService.RemoveAsync(username);
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
-        public Task<Cart> UpdateBasket(Cart cart, DistributedCacheEntryOptions options = null)
+        public async Task<Cart?> GetBasketByUsername(string username)
         {
-            throw new NotImplementedException();
+            var basket = await _redisCacheService.GetStringAsync(username);
+            return string.IsNullOrEmpty(basket) ? null : _serializerService.Deserialize<Cart>(basket);
+        }
+
+        public async Task<Cart> UpdateBasket(Cart cart, DistributedCacheEntryOptions options = null)
+        {
+            if (options != null)
+            {
+                await _redisCacheService.SetStringAsync(cart.Username, _serializerService.Serialize(cart), options);
+            }else{
+                await _redisCacheService.SetStringAsync(cart.Username, _serializerService.Serialize(cart));
+            }
+
+            return await GetBasketByUsername(cart.Username);
         }
     }
 }
