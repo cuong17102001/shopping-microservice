73a2bfbf9a604ea5d1347f1bcf5f6da7721c053f
cuongnq <nqcuong.1710@gmail.com>
2024-09-15 13:15:38 +0700

    CICD push image docker

--- shopping-microservice/src/Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ shopping-microservice/src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -1,16 +1,19 @@
+using Contracts.Common.Events;
 using Contracts.Domains;
 using Ordering.Domain.Enums;
+using Ordering.Domain.OrderAggregate.Events;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ordering.Domain.Entities
 {   
-    public class Order : EntityAuditBase<long>
+    public class Order : AuditableEventEntity<long>
     {
         [Required]
         public string UserName { get; set; }
         [Column(TypeName = "decimal(10,2)")]
         public decimal TotalPrice { get; set; }
+        public Guid DocumentNo { get; set; } = Guid.NewGuid();
         [Required]
         [Column(TypeName = "nvarchar(50)")]
         public string FirstName { get; set; }
@@ -25,5 +28,17 @@ namespace Ordering.Domain.Entities
         [Column(TypeName = "nvarchar(250)")]
         public string InvoiceAddress { get; set; }
         public EOrderStatus Status{ get; set; }
+
+        public Order AddedOrder()
+        {
+            AddDomainEvent(new OrderCreatedEvent(Id, UserName, DocumentNo.ToString(), TotalPrice, ShippingAddress, EmailAddress, InvoiceAddress));
+            return this;
+        }
+
+        public Order DeletedOrder()
+        {
+            RemoveDomainEvent(new OrderDeletedEvent(Id));
+            return this;
+        }
     }
 }
---
+++ shopping-microservice/src/Services/Ordering/Ordering.Domain/OrderAggregate/Events/OrderCreatedEvent.cs
@@ -0,0 +1,33 @@
+﻿using Ordering.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts.Common.Events;
+
+namespace Ordering.Domain.OrderAggregate.Events
+{
+    public class OrderCreatedEvent : BaseEvent
+    {
+        public long Id { get; set; }
+        public string UserName { get; private set; }
+        public string DocumentNo { get; private set; }
+        public decimal TotalPrice { get; private set; }
+        public string ShippingAddress { get; private set; }
+        public string EmailAddress { get; private set; }
+        public string InvoiceAddress { get; private set; }
+
+        public OrderCreatedEvent(long id, string userName, string documentNo, decimal totalPrice, string shippingAddress, string emailAddress, string invoiceAddress) { 
+            Id = id;
+            UserName = userName;
+            DocumentNo = documentNo;
+            TotalPrice = totalPrice;
+            ShippingAddress = shippingAddress;
+            EmailAddress = emailAddress;
+            InvoiceAddress = invoiceAddress;
+        }
+    }
+}
